488d9899a34d345c9066ba5095195ee6eb92c3e2
David Phillips <dphillips@601am.com>
2012-03-14 19:00:33 -0700

    Implement sorting of search results by next entry event recurrence date via SortByEntryEvent=1 query parameter. (Requires search results to have been previously filtered by entry_events search results filter.)

--- plugins/EntryEvents/lib/EntryEvent/Search.pm
+++ plugins/EntryEvents/lib/EntryEvent/Search.pm
@@ -16,10 +16,17 @@ use strict;
 use warnings;
 
 use MT::Util qw( epoch2ts );
+use MT::Request;
 use EntryEvent::EntryEvent;
 use EntryEvent::Util qw( ts2datetime );
 
 require DateTime;
+
+my $orig_execute;
+unless ($orig_execute) {
+    $orig_execute = \&MT::App::Search::execute;
+    *MT::App::Search::execute = \&execute;
+}
 
 sub init_request {
     my $cb = shift;
@@ -29,6 +36,66 @@ sub init_request {
     my $reg = $app->registry( $app->mode, 'types', 'entry' );
     my $filter_types = $reg->{'filter_types'};
     $filter_types->{entry_events} = \&_filter_by_event;
+}
+
+sub execute {
+    my $app = shift;
+    my ( $terms, $args ) = @_;
+
+    my $q = $app->param;
+    my $event_range = $q->param('entry_events');
+    my $sort_by_next_event = $q->param('SortByEntryEvent');
+    unless ($event_range && $sort_by_next_event) {
+        return $orig_execute->($app, @_);
+    }
+
+    my $r = MT::Request->instance;
+    # Get cached $start from MT::Request instance
+    # Calculated and cached by _filter_by_event()
+    my $start = $r->cache('entry_events_start');
+    # Get cached, calculated event dates from MT::Request instance
+    # Calculated and cached by _filter_by_event()
+    my $entry_event_dates = $r->cache('entry_event_dates');
+
+    # Abort sorting if $start does not look like a timestamp
+    return $orig_execute->($app, @_)
+        if ($start !~ m/^\d{14}$/);
+
+    my $class = $app->model( $app->{searchparam}{Type} )
+        or return $app->errtrans( 'Unsupported type: [_1]',
+        encode_html( $app->{searchparam}{Type} ) );
+
+    my $count = $app->count( $class, $terms, $args );
+    return $app->errtrans( "Invalid query: [_1]", $app->errstr )
+        unless defined $count;
+
+    my $offset = $args->{offset} || 0;
+    my $limit = $args->{limit} || 0;
+    delete $args->{offset};
+    delete $args->{limit};
+    my @results = $class->load( $terms, $args )
+        or $app->error($class->errstr);
+    my @ids = map { $_->id } @results;
+
+    my %values = map {
+        $_ => $entry_event_dates->{$_}
+        } @ids;
+
+    my $max;
+    if ($limit) {
+        $max = $limit + $offset - 1;
+    }
+    if (!$max || ($max > $#results)) {
+        $max = $#results;
+    }
+    if ($app->{searchparam}{SearchResultDisplay} eq 'descend') {
+        @results = sort { ($values{$b->id} || 0) cmp ($values{$a->id} || 0) } @results;
+    } else {
+        @results = sort { ($values{$a->id} || 0) cmp ($values{$b->id} || 0) } @results;
+    }
+    @results = @results[$offset .. $max];
+    my $iter = sub { shift @results; };
+    ( $count, $iter );
 }
 
 sub _filter_by_event {
@@ -95,12 +162,20 @@ sub _filter_by_event {
                               )
                         },
                     );
+    my $entry_event_dates = {};
     while ( my $event = $event_iter->() ) {
         my $event_date = $event->get_next_occurrence($start);
         if ( $event_date && ($event_date < $end) ) {
-                push @entry_ids, $event->entry_id;
+            push @entry_ids, $event->entry_id;
+            $entry_event_dates->{ $event->entry_id } = $event_date;
         }
     }
+
+    my $r = MT::Request->instance;
+    # Cache $start in MT::Request instance for later use by execute()
+    $r->cache('entry_events_start', $start);
+    # Cache cacluated event dates in MT::Request instance for later use by execute()
+    $r->cache('entry_event_dates', $entry_event_dates);
 
     # If we have no entry IDs, use "0" as a place holder to prevent query error
     #   but still force no results to be returned
