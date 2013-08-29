5a0038ea2b3094057b7bc1d7caf029af923a4bd1
David Phillips <dphillips@601am.com>
2013-08-29 08:11:11 -0700

    Add basic L10N support - currently English only.

---
+++ plugins/EntryEvents/lib/EntryEvent/L10N/en_us.pm
@@ -0,0 +1,74 @@
+package EntryEvent::L10N::en_us;
+
+use strict;
+
+use base 'EntryEvent::L10N';
+use vars qw( %Lexicon );
+%Lexicon = (
+
+    ## ./plugins/EntryEvents/tmpl/blog_config.tmpl
+
+    ## ./plugins/EntryEvents/tmpl/calendar.tmpl
+    '_LOCALE_WEEK_START' => '00',
+    'Sunday' => 'Sunday',
+    'Monday' => 'Monday',
+    'Tuesday' => 'Tuesday',
+    'Wednesday' => 'Wednesday',
+    'Thursday' => 'Thursday',
+    'Friday' => 'Friday',
+    'Saturday' => 'Saturday',
+    'S|M|T|W|T|F|S' => 'S|M|T|W|T|F|S',
+    'January' => 'January',
+    'Febuary' => 'Febuary',
+    'March' => 'March',
+    'April' => 'April',
+    'May' => 'May',
+    'June' => 'June',
+    'July' => 'July',
+    'August' => 'August',
+    'September' => 'September',
+    'October' => 'October',
+    'November' => 'November',
+    'December' => 'December',
+    'Jan' => 'Jan',
+    'Feb' => 'Feb',
+    'Mar' => 'Mar',
+    'Apr' => 'Apr',
+    '_SHORT_MAY' => 'May',
+    'Jun' => 'Jun',
+    'Jul' => 'Jul',
+    'Aug' => 'Aug',
+    'Sep' => 'Sep',
+    'Oct' => 'Oct',
+    'Nov' => 'Nov',
+    'Dec' => 'Dec',
+    'OK' => 'OK',
+
+    ## ./plugins/EntryEvents/tmpl/events-widget-mt5.tmpl
+    'Events' => 'Events',
+
+    ## ./plugins/EntryEvents/tmpl/events-widget.tmpl
+
+    ## Error messages, strings in the app code.
+
+    ## ./plugins/EntryEvents/extlib/DateTime/Event/ICal.pm
+
+    ## ./plugins/EntryEvents/lib/EntryEvent/App.pm
+
+    ## ./plugins/EntryEvents/lib/EntryEvent/CMS.pm
+
+    ## ./plugins/EntryEvents/lib/EntryEvent/EntryEvent.pm
+
+    ## ./plugins/EntryEvents/lib/EntryEvent/L10N.pm
+
+    ## ./plugins/EntryEvents/lib/EntryEvent/Search.pm
+    'Unsupported type: [_1]' => 'Unsupported type: [_1]', # Translate - New (3)
+    'Invalid query: [_1]' => 'Invalid query: [_1]', # Translate - New (3)
+
+    ## ./plugins/EntryEvents/lib/EntryEvent/Tags.pm
+
+    ## ./plugins/EntryEvents/lib/EntryEvent/Util.pm
+
+);
+
+1;
